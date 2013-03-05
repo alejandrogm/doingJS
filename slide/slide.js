7d335cebda1b02da81bc61ace1da2485bdb3d97d
alejandrogm <mdskhead@gmail.com>
2013-03-05 10:08:04 -0500

    Function buttons add

--- slide/slide.js
+++ slide/slide.js
@@ -2,6 +2,18 @@ var cont = document.getElementById("contslide");
 var totalImages = cont.getElementsByTagName("img").length;
 var elemWidth = 640;
 
+function createButton(){
+	var button = document.createElement("a");
+	button.setAttribute("href", "#");
+	button.setAttribute("alt", "Go to image "+i)
+	button.innerHTML = "Image "+i ;
+	document.getElementById("controls").appendChild(button);
+}
+
+for (var i = 1; i <= totalImages; i++){	
+	createButton();	
+}
+
 function buttonEvents(){
 	var buttonLeft = document.getElementById("left");
 	var buttonRight = document.getElementById("right");
